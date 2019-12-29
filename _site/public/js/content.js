3c81d44a4d63dc75517a867000d92650025823ab
wrl <944514218@qq.com>
2019-12-29 10:35:46 +0800

    最终版

---
+++ _site/public/js/content.js
@@ -0,0 +1,44 @@
+var img = document.getElementsByTagName('img');
+        var a2 = document.getElementById('a2')
+        img[0].onmouseenter =function () {
+            a2.style.left = "830px";
+            a2.style.top = "89px";
+        } 
+        img[1].onmouseenter =function () {
+            a2.style.left = "925px";
+            a2.style.top = "89px";
+        } 
+        img[2].onmouseenter =function () {
+            a2.style.left = "1020px";
+            a2.style.top = "89px";
+        } 
+        img[3].onmouseenter =function () {
+            a2.style.left = "1115px";
+            a2.style.top = "89px";
+        } 
+        img[4].onmouseenter =function () {
+            a2.style.left = "1208px";
+            a2.style.top = "89px";
+        } 
+
+        img[0].onmouseout =function () {
+            a2.style.left = "1010px";
+            a2.style.top = "20px";
+            console.dir("yichu");
+        } 
+        img[1].onmouseout =function () {
+            a2.style.left = "1010px";
+            a2.style.top = "20px";
+        } 
+        img[2].onmouseout =function () {
+            a2.style.left = "1010px";
+            a2.style.top = "20px";
+        } 
+        img[3].onmouseout =function () {
+            a2.style.left = "1010px";
+            a2.style.top = "20px";
+        } 
+        img[4].onmouseout =function () {
+              a2.style.left = "1010px";
+            a2.style.top = "20px";
+        } 
